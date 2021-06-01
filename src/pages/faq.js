cc75643520a10e0505aa08758c7210218fea3c3f
Han Wang <hyw2@cornell.edu>
2021-06-01 00:23:38 -0400

    Add FAQ page

---
+++ src/pages/faq.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import PageWrapper from '../components/PageWrapper';
+
+const AboutPage = () => {
+  return (
+    <>
+      <PageWrapper>
+        <div style={{ padding: '6rem 4rem' }}>
+          <div>
+            What is Dunder Mifflin? Dunder Mifflin Paper Company, Inc. is a
+            paper and office supply sales company. Our slogan is "Limitless
+            Paper in a Paperless World" and we pride ourselves as being the
+            people person's paper people. Compared to larger paper companies,
+            Dunder Mifflin offers reliable customer support and relationships
+            with the sales team.
+          </div>
+          <div>
+            What is the history of Dunder Mifflin? Dunder Mifflin was founded by
+            Robert Dunder and Robert Mifflin, who met while on a Dartmouth
+            College tour. The college buddies became fast friends and went into
+            business together in 1949. Before the company sold paper, Dunder
+            Mifflin profited off of selling parts for construction. Since then,
+            the company has found its niche in the paper and printer industry.
+          </div>
+          <div>
+            How can I buy paper? Dunder Mifflin sells paper for $9.78 a ream,
+            with a current 7% discount. The cost of shipping is $1.00 per 100
+            reams of paper. Dunder Mifflin guarantees free shipping for all
+            orders over 20 reams. We offer a wide variety of paper, varying in
+            sizes, color, and material. Contact us to talk to a sales
+            representative!
+          </div>
+          <div>
+            How can I buy printers? The cost of a Sabre printer is $1,199.99.
+            Our monthly payment plan is $99.99 / month for 12 months. The cost
+            of shipping typically starts at $5.99, depending on the destination.
+          </div>
+          <div>
+            I'm having issues with my printer, what next? If your Sabre printer
+            starts smoking or catches on fire, please contact
+            support@sabreprinters.com to start a return shipment and receive a
+            full refund on your printer.
+          </div>
+          <div>
+            When can I expect my delivery? You can typically expect deliveries
+            to arrive in 3 to 5 days. Dunder Mifflin uses ground delivery to get
+            your orders to you fast and reliably. Please contact your sales
+            representative about tracking your order online.
+          </div>
+          <div>
+            How can I work at Dunder Mifflin? Dunder Mifflin is always looking
+            for people passionate about the paper industry! We offer positions
+            in the sales, warehouse, and office departments. Dunder Mifflin also
+            has temp and intern positions for new grads or high school students.
+          </div>
+        </div>
+      </PageWrapper>
+    </>
+  );
+};
+
+export default AboutPage;
